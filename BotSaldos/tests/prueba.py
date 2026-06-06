8737aeb8df09dee04e62a95d86ee0f2b33272f66
Emanuel Calderoni <calderoniemanuel4@gmail.com>
2026-06-06 01:40:39 -0300

    Remove obsolete banking notes from test file

--- BotSaldos/tests/prueba.py
+++ BotSaldos/tests/prueba.py
@@ -3,14 +3,3 @@ try:
     print(Decimal('1.500.000'))
 except  Exception :
     print ('Error : no se pudo convertir')
-
-# https://onlinebanking.bancogalicia.com.ar/login
-# //*[@id="DocumentNumber"]
-# //*[@id="UserName"]
-# //*[@id="Password"]
-# //*[@id="submitButton"]
-# https://onlinebanking.bancogalicia.com.ar/inicio
-# //*[@id="mainBox"]/h2[1]/strong  saldo
-# //*[@id="LogOut_a"] cierra sesion
-# https://onlinebanking.bancogalicia.com.ar/login
-
